d7cd08c9873ff0ee56f176788621c8d485ca6912
Maxwell Brown <maxwellgbrown@gmail.com>
2018-09-21 11:14:32 -0400

    Add initial setup.py & deploy script

---
+++ deploy.py
@@ -0,0 +1,10 @@
+"""Deployment script for the Curtis Complaint Counter."""
+
+
+def main():
+    """Deploy the curtis complaint lambda."""
+    print("Deploying the Curtis Complaint Counter")
+
+
+if __name__ == '__main__':
+    main()
