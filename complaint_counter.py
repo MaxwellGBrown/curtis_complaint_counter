5ba5894ef85eed80d693c17eecb4f12ac73f4960
Maxwell Brown <maxwellgbrown@gmail.com>
2018-09-21 12:13:52 -0400

    Add quoting of curtis's complaint

--- complaint_counter.py
+++ complaint_counter.py
@@ -1,11 +1,19 @@
 """An AWS Lambda endpoint expecting Slack integration requests."""
+import urllib.parse
 
 
 def lambda_handler(event, context=None):
     """Lamdba endpoint to count curtis's complaints."""
+    request_body = urllib.parse.parse_qs(event['body'])
+
+    # for some reason text comes out as a string in a list
+    command_text = request_body.get('text', [''])[0]
+
+    response_body = f'Curtis Complained!\n\n> {command_text}'
+
     response = {
         'statusCode': 200,
         'headers': dict(),
-        'body': '',
+        'body': response_body,
     }
     return response
--- test_complaint_counter.py
+++ test_complaint_counter.py
@@ -1,8 +1,38 @@
 """Tests for complaint_counter.py"""
+from urllib.parse import urlencode
+
 from complaint_counter import lambda_handler
+
+
+def request_body(**kwargs):
+    """Create a QS request body, like one provided from a slack request."""
+    kwargs.setdefault("token", "ftMJyTJlA4qo8QNY3PVhuYf6")
+    kwargs.setdefault("team_id", "T18UTUL93")
+    kwargs.setdefault("team_domain", "disjointedimages")
+    kwargs.setdefault("channel_id", "D81P2TLNM")
+    kwargs.setdefault("channel_name", "directmessage")
+    kwargs.setdefault("user_id", "U81JYT64Q")
+    kwargs.setdefault("user_name", "maxwellgbrown")
+    kwargs.setdefault("command", "/curtis_complained")
+    kwargs.setdefault("text", "")
+    kwargs.setdefault(
+        "response_url",
+        "https://hooks.slack.com/commands/T18UTUL93/440741347573"
+        "/RSYHp7T18s0uP4eU7hGFdSm6",
+    )
+
+    return urlencode(kwargs)
 
 
 def test_lambda_handler_returns_200():
     """Test that lambda_handler returns a 200 status code."""
-    response = lambda_handler({})
+    response = lambda_handler({'body': request_body()})
     assert response['statusCode'] == 200
+
+
+def test_lambda_handler_quotes_the_complaint():
+    """Test that the lambda_handler returns the complaint as a quote."""
+    complaint = "about foo and bar"
+    response = lambda_handler({"body": request_body(text=complaint)})
+
+    assert f'\n> {complaint}' in response['body']
